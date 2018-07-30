10ba4de1c95d88be05970dfcab6ac2a426a0829a
zhanglei <lei.zhang@chejianer.com>
2018-07-30 14:44:31 +0800

    开发小技巧 判断一个字段如果为空 默认是‘’ ，shell调试 admin行显示详细内容 以及 admin添加过滤器

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from . import models
+
+
 # Register your models here.
 
-#关联到后台
-admin.site.register(models.Article)
+# admin技巧 显示详细内容  添加时间过滤器
+class ArticleAdmin(admin.ModelAdmin):
+    # 显示详细内容
+    list_display = ('title', 'content', 'pub_time')
+    #     添加时间过滤器 元祖只有一个元素的话必须加逗号
+    list_filter = ('pub_time',)
+
+
+# 关联到后台
+admin.site.register(models.Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32, default='title')
     content = models.TextField(null=True)
+    pub_time = models.DateTimeField(null=True)
     #默认对象 改为对象属性 3.0之后
     def __str__(self):
         return self.title
